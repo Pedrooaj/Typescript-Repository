0a5cfc993739bd482d14d3477213bd63a8a48978
Pedrooaj <pedroantonio5735@gmail.com>
2024-09-13 17:22:14 -0300

    update

---
+++ src/Webpack-TS/Aulas-Webpack/aula-1.ts
@@ -0,0 +1,65 @@
+import isEmail from "validator/lib/isEmail";
+
+const inputUser = document.querySelector('#username') as HTMLInputElement;
+const inputEmail = document.querySelector('#email') as HTMLInputElement;
+const inputPassword = document.querySelector('#password') as HTMLInputElement;
+const secondPassword = document.querySelector('#password2') as HTMLInputElement;
+const form = document.querySelector('#form') as HTMLFormElement;
+const error = 'show-error-message';
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    hideErrorMessages(form);
+    checkEmptyField(inputUser, inputEmail, inputPassword, secondPassword);
+    checkEmail(inputEmail);
+    checkPasswords(inputPassword, secondPassword);
+
+    if(sendForm(form)) console.log('Formulario enviado');
+    
+})
+
+
+function checkPasswords(password1: HTMLInputElement, password2: HTMLInputElement): void{
+    if(password1.value != password2.value){
+        showErrorMessage(password1, "As Senhas não batem");
+        showErrorMessage(password2, "As Senhas não batem")
+    }
+}
+function checkEmail(email: HTMLInputElement): void{
+    if(!isEmail(email.value)){
+        showErrorMessage(email, "Email Invalido")
+    }
+
+}
+
+function checkEmptyField(...inputs: HTMLInputElement[]): void{
+    inputs.forEach(input => {
+        if(!input.value){
+            showErrorMessage(input, "Este Campo não pode ficar vazio")
+        }
+    })
+}
+
+
+function hideErrorMessages(form: HTMLFormElement): void{
+    form.querySelectorAll('.' + error).forEach(item => item.classList.remove());
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void{
+    const formFields = input.parentElement as HTMLDivElement;
+    const errorMessage = formFields.querySelector(".error-message") as HTMLSpanElement;
+    errorMessage.innerHTML = msg;
+    formFields.classList.add(error)
+}
+
+function sendForm(form: HTMLFormElement): boolean{
+    let send = true;
+    form.querySelectorAll('.' + error).forEach(() => (send = false));
+    return send;
+}
+
+
+
+
+
---
+++ src/Webpack-TS/index.ts
@@ -0,0 +1,1 @@
+import './Aulas-Webpack/aula-1';
